367bd1af80b6b5352689162b50e9c917196cf3f7
Rose Allegrette <rallegre@umich.edu>
2026-04-20 14:09:42 -0700

    changing the artists to be real artists

--- src/data/activities.ts
+++ src/data/activities.ts
@@ -10,7 +10,7 @@ export const mockActivities: Activity[] = [
     id: 'act1',
     user: mockFriends[0],
     type: 'going',
-    show: mockShows[0],
+    show: mockShows[0], // Chappell Roan
     timestamp: subMinutes(now, 12).toISOString(),
   },
   {
@@ -18,21 +18,21 @@ export const mockActivities: Activity[] = [
     user: mockFriends[1],
     type: 'reacted',
     reactionType: 'fire',
-    show: mockShows[0],
+    show: mockShows[1], // Sabrina Carpenter
     timestamp: subMinutes(now, 28).toISOString(),
   },
   {
     id: 'act3',
     user: mockFriends[3],
     type: 'going',
-    show: mockShows[1],
+    show: mockShows[1], // Sabrina Carpenter
     timestamp: subHours(now, 1).toISOString(),
   },
   {
     id: 'act4',
     user: mockFriends[4],
     type: 'interested',
-    show: mockShows[2],
+    show: mockShows[2], // Wallows
     timestamp: subHours(now, 2).toISOString(),
   },
   {
@@ -40,14 +40,14 @@ export const mockActivities: Activity[] = [
     user: mockFriends[2],
     type: 'reacted',
     reactionType: 'heart',
-    show: mockShows[3],
+    show: mockShows[3], // Tyler, the Creator
     timestamp: subHours(now, 3).toISOString(),
   },
   {
     id: 'act6',
     user: mockFriends[5],
     type: 'going',
-    show: mockShows[3],
+    show: mockShows[4], // Billie Eilish
     timestamp: subHours(now, 4).toISOString(),
   },
   {
@@ -55,14 +55,14 @@ export const mockActivities: Activity[] = [
     user: mockFriends[0],
     type: 'reacted',
     reactionType: 'dancing',
-    show: mockShows[5],
+    show: mockShows[5], // Boygenius
     timestamp: subHours(now, 5).toISOString(),
   },
   {
     id: 'act8',
     user: mockFriends[1],
     type: 'interested',
-    show: mockShows[4],
+    show: mockShows[6], // SZA
     timestamp: subHours(now, 7).toISOString(),
   },
   {
@@ -70,21 +70,21 @@ export const mockActivities: Activity[] = [
     user: mockFriends[3],
     type: 'reacted',
     reactionType: 'mindblown',
-    show: mockShows[2],
+    show: mockShows[7], // Tame Impala
     timestamp: subHours(now, 9).toISOString(),
   },
   {
     id: 'act10',
     user: mockFriends[4],
     type: 'going',
-    show: mockShows[5],
+    show: mockShows[3], // Tyler, the Creator
     timestamp: subHours(now, 12).toISOString(),
   },
   {
     id: 'act11',
     user: mockFriends[2],
     type: 'interested',
-    show: mockShows[6],
+    show: mockShows[5], // Boygenius
     timestamp: subHours(now, 18).toISOString(),
   },
   {
@@ -92,7 +92,7 @@ export const mockActivities: Activity[] = [
     user: mockFriends[5],
     type: 'reacted',
     reactionType: 'guitar',
-    show: mockShows[1],
+    show: mockShows[2], // Wallows
     timestamp: subHours(now, 24).toISOString(),
   },
 ];
